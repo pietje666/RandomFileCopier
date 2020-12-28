cd3747ff4f0bd5a1e0ea2de4013c18a18c6ed9bc
Martijn Moreel <martijn.moreel@tigerconsult.be>
2020-12-28 13:55:53 +0100

    Revert new csproj & packagereference in order to keep the current installation project

--- RandomFileCopier/Properties/AssemblyInfo.cs
+++ RandomFileCopier/Properties/AssemblyInfo.cs
@@ -8,7 +8,7 @@ using System.Windows;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("RandomFileCopier")]
-[assembly: AssemblyDescription("This program makes it possible to copy random Audio/Video files and even Folders")]
+[assembly: AssemblyDescription("This program makes it possible to copy random Audio/Video/Picture files and even Folders")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("RandomFileCopier")]
@@ -41,6 +41,16 @@ using System.Windows;
                                               // app, or any theme specific resource dictionaries)
 )]
 
-
+// Version information for an assembly consists of the following four values:	
+//	
+//      Major Version	
+//      Minor Version 	
+//      Build Number	
+//      Revision	
+//	
+// You can specify all the values or you can default the Build and Revision Numbers 	
+// by using the '*' as shown below:	
+// [assembly: AssemblyVersion("1.0.*")]	
+[assembly: AssemblyVersion("1.2.*")]
 [assembly: NeutralResourcesLanguage("")]
 
